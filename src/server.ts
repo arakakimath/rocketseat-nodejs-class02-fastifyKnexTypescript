4b1963706e344df647cf1f6453cd22f710118aa0
Matheus Arakaki <arakakimath@gmail.com>
2024-10-30 17:04:09 -0400

    adding validation with zod to environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
+
+import { env } from './env'
 import { knex } from './database'
-import crypto from 'node:crypto'
 
 const app = fastify()
 
@@ -12,7 +13,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
