b5f710eec310ae8e72ba33c60863a14cffdbbbca
Matheus Arakaki <arakakimath@gmail.com>
2024-11-02 00:26:37 -0400

    configuring host

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,11 @@ import { env } from "./env"
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0'
   })
-  .then(() => {
+  .then((err, address) => {
+    if (err) {
+      console.log(err)
+    }
     console.log(`HTTP Server Running at port ${env.PORT}!`)
   })
